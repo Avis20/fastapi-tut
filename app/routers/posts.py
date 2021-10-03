ac998b348f14edd75b864e953f38efb2fe286bda
Орлов Ярослав <y.orlov@fonmix.ru>
2021-10-03 23:02:24 +0300

    'master: init commit Вс 03 окт 2021 23:02:24 MSK'

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -20,3 +20,22 @@ async def get_post(post_id: int):
     if not post:
         raise HTTPException(status_code=404, detail="Post not found")
     return post
+
+
+@router.get('/post/list')
+async def list_post(page: int = 1):
+    total_count = await post_utils.get_count_posts()
+    posts = await post_utils.get_posts(page)
+    return {"total_count": total_count, "list": posts}
+
+
+@router.post('/post/update/{post_id}', response_model=PostDetailsModel)
+async def update_post(post_id: int, post_data: PostModel, current_user: User = Depends(get_current_user)):
+    post = await post_utils.get_post(post_id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    if post.get("user_id") != current_user.get('id'):
+        raise HTTPException(status_code=403, detail="Access Denied")
+    await post_utils.update_post(post_id, post_data)
+    post = await post_utils.get_post(post_id)
+    return post
--- app/utils/posts.py
+++ app/utils/posts.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy.sql import select
+from sqlalchemy.sql import select, desc, func
 
 import app.schemas.posts as post_schema
 from app.models.posts import posts_table
@@ -9,16 +9,18 @@ from app.models.database import database
 
 async def create_post(post: post_schema.PostModel, user):
     query = (
-        posts_table.insert().values(
+        posts_table.insert()
+        .values(
             title=post.title,
             content=post.content,
             ts_create=datetime.now(),
-            user_id=user.get('id')
-        ).returning(
+            user_id=user.get("id"),
+        )
+        .returning(
             posts_table.c.id,
             posts_table.c.title,
             posts_table.c.content,
-            posts_table.c.ts_create
+            posts_table.c.ts_create,
         )
     )
     print("\n\ncreate_post->>>\n")
@@ -27,23 +29,71 @@ async def create_post(post: post_schema.PostModel, user):
     # объект post возвращает ключи, post.values - значения
     # zip и dict превращают в словарь
     post = dict(zip(post, post.values()))
-    post["user_name"] = user.get('name')
+    post["user_name"] = user.get("name")
     return post
 
 
 async def get_post(post_id):
     query = (
-        select([
-            posts_table.c.id,
-            posts_table.c.title,
-            posts_table.c.content,
-            posts_table.c.ts_create,
-            users_table.c.name.label("user_name")
-        ]).select_from(posts_table.join(users_table)).where(
-            posts_table.c.id == post_id
+        select(
+            [
+                posts_table.c.id,
+                posts_table.c.title,
+                posts_table.c.content,
+                posts_table.c.ts_create,
+                posts_table.c.user_id,
+                users_table.c.name.label("user_name"),
+            ]
         )
+        .select_from(posts_table.join(users_table))
+        .where(posts_table.c.id == post_id)
     )
     print("\n\nget_post->>>\n")
     print(query)
     post = await database.fetch_one(query)
     return post
+
+
+async def get_posts(page: int):
+    records_per_page = 3
+    offset = (page - 1) * records_per_page
+    query = (
+        select(
+            [
+                posts_table.c.id,
+                posts_table.c.title,
+                posts_table.c.content,
+                posts_table.c.ts_create,
+                users_table.c.name.label("user_name"),
+            ]
+        )
+        .select_from(posts_table.join(users_table))
+        .order_by(desc(posts_table.c.ts_create))
+        .limit(records_per_page)
+        .offset(offset)
+    )
+    print("\n\nget_posts->>>\n")
+    print(query)
+    posts = await database.fetch_all(query)
+    return posts
+
+
+async def get_count_posts():
+    query = (select([func.count()]).select_from(posts_table))
+    print("\n\nget_count_posts->>>\n")
+    print(query)
+    total = await database.fetch_val(query)
+    return total
+
+
+async def update_post(post_id, post):
+    query = (
+        posts_table.update()
+        .where(posts_table.c.id == post_id)
+        .values(title=post.title, content=post.content)
+    )
+    print("\n\nupdate_posts->>>\n")
+    print(query)
+    post = await database.fetch_one(query)
+    return post
+
